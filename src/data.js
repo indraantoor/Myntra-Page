86715330da3f5982dd894adbb1bfd396e024e774
Indraan S. Toor <indraantoor@gmail.com>
2021-12-21 00:13:49 +0530

    Add: thumbnail urls

--- src/data.js
+++ src/data.js
@@ -7,6 +7,16 @@ const items = [
     originalPrice: 1999,
     discount: 65,
     gender: "men",
+    thumbnailUrl:
+      "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/10863048/2021/11/23/3ac26958-82fc-47e0-ae38-ef2e191d84971637645503435-Roadster-Men-Navy-Blue-Solid-Casual-Shirt-4701637645502688-1.jpg",
+    images: [
+      "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/10863048/2021/11/23/3ac26958-82fc-47e0-ae38-ef2e191d84971637645503435-Roadster-Men-Navy-Blue-Solid-Casual-Shirt-4701637645502688-1.jpg",
+      "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/10863048/2021/11/23/6e81480b-8f92-4be6-9108-f5db344d28c11637645503425-Roadster-Men-Navy-Blue-Solid-Casual-Shirt-4701637645502688-2.jpg",
+      "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/10863048/2021/11/23/7a2646ea-13eb-4b9d-a96d-a46fa88a48591637645503413-Roadster-Men-Navy-Blue-Solid-Casual-Shirt-4701637645502688-3.jpg",
+      "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/10863048/2021/11/23/d52df60b-12d7-46aa-8d0f-d7e5cd1de4ba1637645503402-Roadster-Men-Navy-Blue-Solid-Casual-Shirt-4701637645502688-4.jpg",
+      "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/10863048/2021/11/23/6b212fe2-bd5f-400b-9f4f-0f061b98be261637645503392-Roadster-Men-Navy-Blue-Solid-Casual-Shirt-4701637645502688-5.jpg",
+      "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/10863048/2021/11/23/a82ca8ba-2d2c-4076-9433-83ebee017a6d1637645503383-Roadster-Men-Navy-Blue-Solid-Casual-Shirt-4701637645502688-6.jpg",
+    ],
   },
   {
     id: 2,
@@ -16,6 +26,8 @@ const items = [
     originalPrice: 1249,
     discount: 64,
     gender: "men",
+    thumbnailUrl:
+      "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/9951805/2021/4/20/04874025-1eca-4f03-85af-e9247c539c521618891640521-Roadster-Men-Maroon--Olive-Green-Checked-Casual-Shirt-801618-1.jpg",
   },
   {
     id: 3,
@@ -24,6 +36,8 @@ const items = [
     sellingPrice: 299,
     originalPrice: 1499,
     discount: 60,
+    thumbnailUrl:
+      "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/11549510/2020/2/28/bd54b771-8b3f-4a01-84c9-68efe76fd4dd1582876765649-SASSAFRAS-Women-Black-Regular-Fit-Self-Design-Casual-Shirt-3-1.jpg",
     gender: "women",
   },
   {
@@ -33,6 +47,8 @@ const items = [
     sellingPrice: 494,
     originalPrice: 1099,
     discount: 55,
+    thumbnailUrl:
+      "https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/16154710/2021/11/18/a4c56a5b-14bf-45a3-83a1-722e441d6f9f1637214550886SHAYEWomenGreenOpaqueCasualShirt1.jpg",
     gender: "women",
   },
 ];
